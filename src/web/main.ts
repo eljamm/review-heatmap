52d5a21c2e8f26ec294e3a814bd29d2f51c361b6
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-08 10:19:07 +0200

    Build anki-review-heatmap.js from source

--- src/web/main.ts
+++ src/web/main.ts
@@ -1,4 +1,4 @@
-/* 
+/*
 Review Heatmap Add-on for Anki
 
 Copyright (C) 2016-2022  Aristotelis P. <https//glutanimate.com/>
@@ -29,8 +29,12 @@ listed here: <https://glutanimate.com/contact/>.
 Any modifications to this file must keep this entire header intact.
 */
 
-import "./_vendor/cal-heatmap.css";
-import "./css/review-heatmap.css";
+import calHeatmapCss from "./_vendor/cal-heatmap.css";
+import reviewHeatmapCss from "./css/review-heatmap.css";
+
+var __vite_style__ = document.createElement('style');
+__vite_style__.textContent = calHeatmapCss + "\n" + reviewHeatmapCss;
+document.head.appendChild(__vite_style__);
 
 import { CalHeatMap } from "./_vendor/cal-heatmap.js";
 import { ReviewHeatmapOptions, ReviewHeatmapData } from "./types";
